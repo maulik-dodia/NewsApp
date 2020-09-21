826e817322052032c02015a9dc3ffb6131883f06
maulik-dodia <mmdoida@gmail.com>
2020-09-21 15:44:25 +0530

    Made API Call...

--- app/src/main/java/com/example/newsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/NewsViewModel.kt
@@ -1,7 +1,36 @@
 package com.example.newsapp.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.newsapp.models.NewsResponse
 import com.example.newsapp.repository.NewsRepository
+import com.example.newsapp.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(val newsRepo: NewsRepository) : ViewModel() {
+
+    val breakingNewsLiveData: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val pageNumber = 1
+
+    init {
+        getBreakingNews("in")
+    }
+
+    fun getBreakingNews(countryCode: String) =
+        viewModelScope.launch {
+            breakingNewsLiveData.postValue(Resource.Loading())
+            val response = newsRepo.getBreakingNews(countryCode, pageNumber)
+            breakingNewsLiveData.postValue(handleResponse(response))
+        }
+
+    private fun handleResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { newsResponse ->
+                return Resource.Success(newsResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
--- app/src/main/java/com/example/newsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/BreakingNewsFragment.kt
@@ -1,19 +1,63 @@
 package com.example.newsapp.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.R
+import com.example.newsapp.adapters.NewsAdapter
 import com.example.newsapp.ui.NewsActivity
 import com.example.newsapp.ui.NewsViewModel
+import com.example.newsapp.util.Resource
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        viewModel.breakingNewsLiveData.observe(viewLifecycleOwner, Observer { response ->
+            when (response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { successResponse ->
+                        newsAdapter.differ.submitList(successResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e("TAG", "An error occured $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
     }
 }

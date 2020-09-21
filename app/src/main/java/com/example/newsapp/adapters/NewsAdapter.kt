e6dee921957be4d772048b45e8b9cdbad10040bd
maulik-dodia <mmdoida@gmail.com>
2020-09-21 14:10:25 +0530

    1. Created Database, Converters and RecyclerView Adapter classes 2. Created appropriate packages

---
+++ app/src/main/java/com/example/newsapp/adapters/NewsAdapter.kt
@@ -0,0 +1,70 @@
+package com.example.newsapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.newsapp.R
+import com.example.newsapp.models.Article
+import kotlinx.android.synthetic.main.item_article.view.*
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    private val differCallBack = object : DiffUtil.ItemCallback<Article>() {
+
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallBack)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_article,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+
+        val currArticle = differ.currentList[position]
+
+        holder.itemView.apply {
+
+            Glide.with(this).load(currArticle.urlToImage).into(ivArticleImage)
+
+            tvSource.text = currArticle.title
+            tvDescription.text = currArticle.description
+            tvPublishedAt.text = currArticle.publishedAt
+
+            setOnItemClickListener {
+                onItemClickListener?.let {
+                    it(currArticle)
+                }
+            }
+        }
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    private fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
